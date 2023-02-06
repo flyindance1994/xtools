c050b59146c41124819f3ef1038f2cf592f1bb4a
Eric D <flyindancedz@gmail.com>
2023-02-06 10:53:08 +0800

    feat:array compare

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { program } = require('commander')
-const { md5File, md5CompareByDir, encodeFunc, decodeFunc} = require('./utils');
+const { md5File, md5CompareByDir, encodeFunc, decodeFunc } = require('./utils');
 const moment = require('moment-timezone');
 
 program.version('0.0.1')
@@ -10,6 +10,7 @@ program.option('-t, --timestamp [timestamp]', '将时间戳转换为时间')
 program.option('-m, --md5File [file...]', '生成文件md5,支持多个')
 program.option('-c, --compareMd5 [dir...]', '比较两个目录下所有同名文件md5')
 program.option('-s, --str [str]', '字符串处理 base64 & urlencode')
+program.option('-x, --x [x...]', '数组比较')
 
 program.parse(process.argv)
 
@@ -27,26 +28,26 @@ if (options.timestamp) {
 
     //如果是时间戳
     if (timestampRegExp.test(options.timestamp)) {
-        let timestamp = options.timestamp * 1000; 
+        let timestamp = options.timestamp * 1000;
         const format = "zz YYYY-MM-DD HH:mm:ss"
 
         timezoneList.forEach(item => {
-             result_array.push({
+            result_array.push({
                 title: moment(timestamp).tz(item).format(format),
                 subtitle: item,
                 arg: moment(timestamp).tz(item).format(format)
-             })
+            })
         })
     }
 
-    if(dateRegExp.test(options.timestamp) || datetimeRegExp.test(options.timestamp)) {
+    if (dateRegExp.test(options.timestamp) || datetimeRegExp.test(options.timestamp)) {
         timezoneList.forEach(item => {
             result_array.push({
-               title: moment.tz(options.timestamp, item).unix(),
-               subtitle: item + ' UNIX Timestamp',
-               arg: moment.tz(options.timestamp, item).unix()
+                title: moment.tz(options.timestamp, item).unix(),
+                subtitle: item + ' UNIX Timestamp',
+                arg: moment.tz(options.timestamp, item).unix()
             })
-       })
+        })
     }
 
     console.log(JSON.stringify({
@@ -71,25 +72,46 @@ if (options.str) {
 
     let encode = encodeFunc(options.str)
     let decode = decodeFunc(options.str)
-    
-    for(let key in encode) {
+
+    for (let key in encode) {
         result_array.push({
             title: encode[key],
             subtitle: key,
             arg: encode[key]
-         })
+        })
     }
 
-    for(let key in decode) {
+    for (let key in decode) {
         result_array.push({
             title: decode[key],
             subtitle: key,
             arg: decode[key]
-         })
+        })
     }
 
     console.log(JSON.stringify({
         items: result_array
     }))
+}
+
+if (options.x) {
+    if (Object.keys(options.x).length !== 2) {
+        console.error("ERROR:only 2 str can be accepted")
+        return
+    }
+
+    let arrA = new Array()
+    let arrB = new Array()
+    arrA = options.x[0].split(",")
+    arrB = options.x[1].split(",")
+
+    console.log("common items A & B :")
+    console.log(arrA.filter(item => arrB.includes(item)))
+
+    console.log("item of A not in B:")
+    console.log(arrA.filter(item => !arrB.includes(item)))
+
+    console.log("item of B not in A:")
+    console.log(arrB.filter(item => !arrA.includes(item)))
 }
 
